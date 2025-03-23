acdce9bf0025bf382b06a3f4e2d8385227bd0b82
Elisa <elisa2b@hotmail.com>
2025-03-23 21:20:21 +0000

    add distribution test

--- src/statistical_test.py
+++ src/statistical_test.py
@@ -1,7 +1,8 @@
-from scipy.stats import ttest_rel, wilcoxon
+from scipy.stats import ttest_rel, wilcoxon, shapiro
 import numpy as np
 import pandas as pd
 import ast  # To convert string lists to actual lists
+
 
 ##########  Download & read data ##########
 path = f'results/experiments.csv'
@@ -16,17 +17,26 @@ half = len(all_accuracies) // 2  # Find the middle index
 baseline_accuracies = np.array(all_accuracies[:half])  # First half
 svm_accuracies = np.array(all_accuracies[half:])  # Second half
 
-# Perform paired t-test
-t_stat, p_ttest = ttest_rel(svm_accuracies, baseline_accuracies)
+# Check whether differences between paired values
+# (SVM accuracy - Baseline accuracy) follow a normal distribution.
+stat, p = shapiro(svm_accuracies - baseline_accuracies)
+print(f"\nShapiro-Wilk test p-value: {p}")
 
-# Perform Wilcoxon signed-rank test
-w_stat, p_wilcoxon = wilcoxon(svm_accuracies, baseline_accuracies)
+if p > 0.05:
+    # Perform paired t-test
+    t_stat, p_ttest = ttest_rel(svm_accuracies, baseline_accuracies)
+    print(f"\nThe Shapiro-Wilk test p_value is superior to 0.05, thus the differences between paired values\n(SVM accuracies-Baseline accuracies) follow a normal distribution. \nWe can use paired t-test.\n")
+    print(f"\nPaired t-test:\n     t-statistic = {t_stat}\n     p-value = {p_ttest}\n")
+    p_value = p_ttest
+else:
+    # Perform Wilcoxon signed-rank test
+    w_stat, p_wilcoxon = wilcoxon(svm_accuracies, baseline_accuracies)
+    print(
+        f"\nThe Shapiro-Wilk test p_value is inferior to 0.05, thus the differences between paired values\n(SVM accuracies-Baseline accuracies) don't follow a normal distribution. \nWe can use Wilcoxon signed-rank test.\n")
+    print(f"Wilcoxon signed-rank test:\n     W-statistic = {w_stat}\n     p-value = {p_wilcoxon}\n")
+    p_value= p_wilcoxon
 
-# Print results
-print(f"\nPaired t-test:\n     t-statistic = {t_stat}\n     p-value = {p_ttest}\n")
-print(f"Wilcoxon signed-rank test:\n     W-statistic = {w_stat}\n     p-value = {p_wilcoxon}\n")
-
-if p_ttest < 0.05 and p_wilcoxon < 0.05:
+if p_value < 0.05 :
     print(f"The p-value is inferior to 0.05, the difference in accuracy is statistically significant.\n")
 else :
     print(f"The p-value is superior to 0.05, the difference in accuracy is not statistically significant.\n")
